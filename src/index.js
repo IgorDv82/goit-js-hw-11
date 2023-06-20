1cb791686513ede8077862d12e0e5763ac7cd429
Igor Dorianskyi <dis.igor@gmail.com>
2023-06-20 19:32:05 +0300

    fixed bug with loading last page

--- src/index.js
+++ src/index.js
@@ -8,13 +8,14 @@ import './CSS/style.css';
 const formEl = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery');
 const loadMore = document.querySelector('.load-more');
-const body = document.querySelector('body');
+const endOfSearch = document.querySelector('.theEnd');
 loadMore.classList.add('is-hidden');
+endOfSearch.classList.add('is-hidden');
 
 let query = '';
 let page = 1;
 const per_page = 40;
-let loadedPhoto = 0;
+let lastPage = 0;
 
 formEl.addEventListener('submit', hendlerSubmit);
 loadMore.addEventListener('click', hendlerClick);
@@ -39,49 +40,13 @@ function renderMarkup(data) {
   loadedPhoto = page * per_page;
 
   if (loadedPhoto <= totalHits) {
-    const markup = hits
-      .map(
-        ({
-          webformatURL,
-          largeImageURL,
-          tags,
-          likes,
-          views,
-          comments,
-          downloads,
-        }) => `<div class="photo-card">
-  <a href="${largeImageURL}" class="link">
-  <img class="photo" src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
-  <div class="info">
-    <p class="info-item">
-      <b>Likes</b>
-      <span>${likes}</span>
-    </p>
-    <p class="info-item">
-      <b>Views</b>
-      <span>${views}</span>
-    </p>
-    <p class="info-item">
-      <b>Comments</b>
-      <span>${comments}</span>
-    </p>
-    <p class="info-item">
-      <b>Downloads</b>
-      <span>${downloads}</span>
-    </p>
-  </div>
-</div>`
-      )
-      .join('');
-    gallery.insertAdjacentHTML('beforeend', markup);
+    makeMarkup(hits);
     lightbox.refresh();
     loadMore.classList.remove('is-hidden');
   } else {
+    makeMarkup(hits);
     loadMore.classList.add('is-hidden');
-    const text = `<p class="theEnd">
-        We're sorry, but you've reached the end of search results.
-      </p>`;
-    body.insertAdjacentHTML('beforeend', text);
+    endOfSearch.classList.remove('is-hidden');
   }
 }
 
@@ -106,6 +71,7 @@ function resetAll() {
   formEl.reset();
   gallery.innerHTML = '';
   loadMore.classList.add('is-hidden');
+  endOfSearch.classList.add('is-hidden');
   page = 1;
 }
 
@@ -113,5 +79,43 @@ function hendlerClick(e) {
   page += 1;
   getPictures(query, page)
     .then(data => renderMarkup(data))
-    .catch(e => Report.failure('Sorry...', 'Please try again.'));
+    .catch(err => Report.failure('Sorry...', 'Please try again.'));
+}
+
+function makeMarkup(arr) {
+  const markup = arr
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `<div class="photo-card">
+  <a href="${largeImageURL}" class="link">
+  <img class="photo" src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+      <span>${likes}</span>
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+      <span>${views}</span>
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+      <span>${comments}</span>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+      <span>${downloads}</span>
+    </p>
+  </div>
+</div>`
+    )
+    .join('');
+  gallery.insertAdjacentHTML('beforeend', markup);
 }
