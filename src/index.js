6f76f51903f2d4365afa09740ff799b3143a314f
Igor Dorianskyi <dis.igor@gmail.com>
2023-06-26 21:38:10 +0300

    second

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { getPictures, PER_PAGE } from './pixabay-api';
+import { getPictures } from './pixabay-api';
 import { Report } from 'notiflix/build/notiflix-report-aio';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
@@ -8,11 +8,14 @@ import './CSS/style.css';
 const formEl = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery');
 const loadMore = document.querySelector('.load-more');
-const endOfSearch = document.querySelector('.theEnd');
+const body = document.querySelector('body');
+loadMore.classList.add('is-hidden');
 
 let query = '';
 let page = 1;
-console.log(PER_PAGE);
+const per_page = 40;
+let loadedPhoto = 0;
+
 formEl.addEventListener('submit', hendlerSubmit);
 loadMore.addEventListener('click', hendlerClick);
 
@@ -33,64 +36,20 @@ function renderMarkup(data) {
       timeout: 1000,
     });
   }
-  loadedPhoto = page * PER_PAGE;
+  loadedPhoto = page * per_page;
 
   if (loadedPhoto <= totalHits) {
-    makeMarkup(hits);
-    lightbox.refresh();
-    loadMore.classList.remove('is-hidden');
-  } else {
-    makeMarkup(hits);
-    lightbox.refresh();
-    loadMore.classList.add('is-hidden');
-    endOfSearch.classList.remove('is-hidden');
-  }
-}
-
-function hendlerSubmit(e) {
-  e.preventDefault();
-  query = e.currentTarget.elements.searchQuery.value;
-  if (query.trim() === '') {
-    formEl.reset();
-    gallery.innerHTML = '';
-    page = 1;
-    loadMore.classList.add('is-hidden');
-    endOfSearch.classList.add('is-hidden');
-    Notify.info('Please, fill in key-word for searching.', {
-      position: 'center-center',
-      timeout: 1000,
-    });
-    return;
-  }
-  gallery.innerHTML = '';
-  page = 1;
-  getPictures(query, page)
-    .then(data => renderMarkup(data))
-    .catch(e => Report.failure('Sorry...', 'Please try again.'));
-  formEl.reset();
-  loadMore.classList.add('is-hidden');
-  endOfSearch.classList.add('is-hidden');
-}
-
-function hendlerClick(e) {
-  page += 1;
-  getPictures(query, page)
-    .then(data => renderMarkup(data))
-    .catch(err => Report.failure('Sorry...', 'Please try again.'));
-}
-
-function makeMarkup(arr) {
-  const markup = arr
-    .map(
-      ({
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) => `<div class="photo-card">
+    const markup = hits
+      .map(
+        ({
+          webformatURL,
+          largeImageURL,
+          tags,
+          likes,
+          views,
+          comments,
+          downloads,
+        }) => `<div class="photo-card">
   <a href="${largeImageURL}" class="link">
   <img class="photo" src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
   <div class="info">
@@ -112,7 +71,47 @@ function makeMarkup(arr) {
     </p>
   </div>
 </div>`
-    )
-    .join('');
-  gallery.insertAdjacentHTML('beforeend', markup);
+      )
+      .join('');
+    gallery.insertAdjacentHTML('beforeend', markup);
+    lightbox.refresh();
+    loadMore.classList.remove('is-hidden');
+  } else {
+    loadMore.classList.add('is-hidden');
+    const text = `<p class="theEnd">
+        We're sorry, but you've reached the end of search results.
+      </p>`;
+    body.insertAdjacentHTML('beforeend', text);
+  }
+}
+
+function hendlerSubmit(e) {
+  e.preventDefault();
+  query = e.currentTarget.elements.searchQuery.value;
+  if (query.trim() === '') {
+    resetAll();
+    Notify.info('Please, fill in key-word for searching.', {
+      position: 'center-center',
+      timeout: 1000,
+    });
+    return;
+  }
+  resetAll();
+  getPictures(query, page)
+    .then(data => renderMarkup(data))
+    .catch(e => Report.failure('Sorry...', 'Please try again.'));
+}
+
+function resetAll() {
+  formEl.reset();
+  gallery.innerHTML = '';
+  loadMore.classList.add('is-hidden');
+  page = 1;
+}
+
+function hendlerClick(e) {
+  page += 1;
+  getPictures(query, page)
+    .then(data => renderMarkup(data))
+    .catch(e => Report.failure('Sorry...', 'Please try again.'));
 }
--- src/pixabay-api.js
+++ src/pixabay-api.js
@@ -4,7 +4,7 @@ const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '37473288-0ea8494b0f878a1b6134977d3';
 const IMAGE_TYPE = 'photo';
 const ORIENTATION = 'horizontal';
-export const PER_PAGE = 40;
+const PER_PAGE = 40;
 
 export async function getPictures(request = '', loadPage = 1) {
   return await axios
